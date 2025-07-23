d507e047ed8a5cb67c73c05dbd744069b3dde717
mariyadyshkant <mariya.dyshkant@gmail.com>
2025-07-23 21:39:36 +0200

    refactor main component

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,15 +1,21 @@
 import comics from '../data/comics.js'
+import BtnLoadMore from './Btn-load-more.jsx';
 
 export default function Main() {
     return (
         <main>
             <div className="container">
                 <div className="row">
-                    {comics.map(({ thumb, title }) => (
-                        <div className="col-6" key={title}>
-
+                    {comics.map(({ id, thumb, title, price }) => (
+                        <div className="col-6" key={id}>
+                            <img src={thumb} alt={title} />
+                            <h3>{title}</h3>
+                            <p>{price}</p>
                         </div>
                     ))}
+                </div>
+                <div className="row-btn-load-more">
+                    <BtnLoadMore />
                 </div>
             </div>
         </main>
